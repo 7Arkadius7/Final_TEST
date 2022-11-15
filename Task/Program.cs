baa031ffd07d3a91950152fb8554428b1be375b2
Arcadiy <Motor-person@yandex.ru>
2022-11-15 11:18:58 +0400

    Решили задание

---
+++ Task/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+string[] startArray = new string[10] {"Geek", "for", "fire", "if", "77", "Box", "Clear", "data", "info", "moon"};
+string[] finishArray = new string[startArray.Length];
+
+void SortedArray(string[] startArray, string[] finishArray)
+{
+    int count = 0;
+    for (int i = 0; i < startArray.Length; i++)
+    {
+    if(startArray[i].Length <= 3)
+        {
+        finishArray[count] = startArray[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+SortedArray(startArray, finishArray);
+PrintArray(finishArray);
